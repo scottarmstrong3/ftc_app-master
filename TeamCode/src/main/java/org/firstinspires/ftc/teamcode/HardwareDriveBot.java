85218c9c0a0f19c4868d7724f81f476f502aa332
JochenFischer <jochen.elon@gmail.com>
2016-10-03 19:08:23 -0400

    SensorLoop
    Added touch, light and gyro sensor to the hardware class. Wrote
    SensorLoop program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDriveBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDriveBot.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -22,6 +25,9 @@ public class HardwareDriveBot
     /* Public OpMode members. */
     public DcMotor  motorLeft   = null;
     public DcMotor  motorRight  = null;
+    public TouchSensor sensorTouch = null;
+    public ColorSensor sensorColor = null;
+    public GyroSensor sensorGyro = null;
 
     // useful constants:
     public static final double SLOW_POWER = 0.2;
@@ -61,6 +67,15 @@ public class HardwareDriveBot
         // Set all motors to run with encoders.
         motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        /**
+         * Setup the sensors
+         */
+        sensorTouch = hwMap.touchSensor.get("sensorTouch");
+        sensorColor = hwMap.colorSensor.get("sensorColor");
+        sensorGyro = hwMap.gyroSensor.get("sensorGyro");
+
+        sensorColor.enableLed(true);
     }
 
     // Stop both motors
